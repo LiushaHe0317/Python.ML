8f21346c02089d69cc79c53fc16e600aa93394ac
Liusha He <37186080+LiushaHe0317@users.noreply.github.com>
2019-03-17 22:56:10 +0000

    Add files via upload

---
+++ #5 POS_NER/POS Dataset/pos_base.py
@@ -0,0 +1,35 @@
+import numpy as np
+import theano
+import theano.tensor as T
+import matplotlib.pyplot as plt
+from sklearn.utils import shuffle
+from sklearn.metrics import f1_score
+from sklearn.tree import DecisionTreeClassifier
+
+class LogisticReg:
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def score(self):
+        pass
+
+    def F1_score(self):
+        pass
+
+def get_data():
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
+
+doc = "how are you degere si"
+from keras.preprocessing.text import Tokenizer
+tokenizer = Tokenizrr()
+output = tokenizer(doc)
+print(output)
